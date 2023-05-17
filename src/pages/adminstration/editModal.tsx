7b51c6ebb88cbac9f1584ddffa6599332a570bdb
ziadAshraf7 <ziadashraf454@icloud.com>
2023-05-17 02:01:14 +0200

    up

--- src/pages/adminstration/editModal.tsx
+++ src/pages/adminstration/editModal.tsx
@@ -79,82 +79,82 @@ export default function EditModal({
 
     if (addStatus) {
         return (
-            <div ref={targetRef} className='absolute p-4 z-10 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
+            <form onSubmit={async () => {
+                if (newAddedFloorName.length) {
+                    try {
+                        await addFloor({ name: newAddedFloorName })
+                        alert("action success")
+                        window.location.reload()
+                    } catch {
+                        alert("error")
+                    }
+                }
+            }} ref={targetRef} className='absolute p-4 z-10 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
 
                 <div onClick={() => setEdit(false)} className='absolute text-white cursor-pointer px-1 top-[3px] left-[3px] rounded-full border-2 border-grey-500'>x</div>
 
                 <div className='mb-4'>
-                    <input className='w-[167px]' onChange={(e) => setNewAddedFloorName(e.target.value)} value={newFloorName} type='text' />
+                    <input required className='w-[167px]' onChange={(e) => setNewAddedFloorName(e.target.value)} value={newFloorName} type='text' />
                     <span className='text-white p-2'>{t("floor")}</span>
                 </div>
 
-                <div onClick={async () => {
-                    if (newAddedFloorName.length) {
-                        try {
-                            await addFloor({ name: newAddedFloorName })
-                            alert("action success")
-                            window.location.reload()
-                        } catch {
-                            alert("error")
-                        }
-                    }
-                }} className='absolute bottom-[5px] right-[5px] text-white'>
+                <button type='submit' className='absolute bottom-[5px] right-[5px] text-white'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
 
-                </div>
-            </div>
+                </button>
+            </form>
         )
     }
 
 
     return (
-        <div ref={targetRef} className='absolute p-4 z-10 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
+        <form onSubmit={async () => {
+            if (checkCode() == "employee") {
+                try {
+                    await handleUpdateEmployee()
+                    alert("action success")
+                    window.location.reload()
+                } catch {
+                    alert("err")
+                }
+            } else {
+                try {
+                    await handleUpdateoffices()
+                    alert("action success")
+                    window.location.reload()
+                } catch {
+                    alert("err")
+                }
+            }
+            return
+        }} ref={targetRef} className='absolute p-4 z-10 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
 
             <div onClick={() => setEdit(false)} className='absolute text-white cursor-pointer px-1 top-[3px] left-[3px] rounded-full border-2 border-grey-500'>x</div>
 
             <div className='mb-4'>
-                <input className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
+                <input required className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
                 <span className='text-white p-2'>{t("name")}</span>
             </div>
 
             <div className='mb-4'>
-                <input className='w-[167px]' onChange={(e) => setFloorName(e.target.value)} value={newFloorName} type='text' />
+                <input required className='w-[167px]' onChange={(e) => setFloorName(e.target.value)} value={newFloorName} type='text' />
                 <span className='text-white p-2'>{t("floor")}</span>
             </div>
 
             <div className=''>
-                <input className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
+                <input required className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
                 <span className='text-white p-2'>{t("office number")}</span>
             </div>
 
 
-            <div onClick={async () => {
-                if (checkCode() == "employee") {
-                    try {
-                        await handleUpdateEmployee()
-                        alert("action success")
-                        window.location.reload()
-                    } catch {
-                        alert("err")
-                    }
-                } else {
-                    try {
-                        await handleUpdateoffices()
-                        alert("action success")
-                        window.location.reload()
-                    } catch {
-                        alert("err")
-                    }
-                }
-                return
-            }} className='absolute bottom-[5px] right-[5px] text-white'>
+            <button type='submit' className='absolute bottom-[5px] right-[5px] text-white'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
 
-            </div>
-        </div >
+            </button>
+        </form >
     )
 }
--- src/pages/edit/editModal.tsx
+++ src/pages/edit/editModal.tsx
@@ -38,21 +38,18 @@ export default function EditModal({
     add: any
 }) {
 
+    const { t } = useTranslation();
     let [newFloorName, setFloorName] = useState(floorName)
     let [newCode, setCode] = useState(addStatus ? "" : code)
     let [newName, setNewName] = useState(addStatus ? "" : name)
-    let [type, setType] = useState("")
+    let [type, setType] = useState(t("employee"))
 
-    const { t } = useTranslation();
 
 
     let codeData = {
         prev: code,
         new: newCode
     }
-
-
-    console.log(codeData)
 
     let nameData = {
         prev: floorName,
@@ -100,9 +97,8 @@ export default function EditModal({
 
     async function handleOpen() {
         try {
-            await add({ floor: floorName, type: type, code, newCode, name: newName })
+            await add({ floor: floorName, type: type, code: newCode, name: newName })
             alert("action success")
-            window.location.reload()
         } catch {
             alert("err")
         }
@@ -115,17 +111,17 @@ export default function EditModal({
                 <div onClick={() => setEdit(false)} className='absolute text-white cursor-pointer px-1 top-[3px] left-[3px] rounded-full border-2 border-grey-500'>x</div>
 
                 <div className='mb-4'>
-                    <input className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
+                    <input required className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
                     <span className='text-white p-2'> {t("name")} </span>
                 </div>
 
                 <div className='mb-4'>
-                    <input className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
+                    <input required className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
                     <span className='text-white p-2'> {t("office number")}</span>
                 </div>
 
                 <div className='mb-4 gap-2'>
-                    <select onChange={(e) => setType(e.target.value)}>
+                    <select required onChange={(e) => setType(e.target.value)}>
                         <option>{t('employee')}</option>
                         <option>{t('office')}</option>
                     </select>
@@ -151,7 +147,7 @@ export default function EditModal({
                 <div onClick={() => setEdit(false)} className='absolute text-white cursor-pointer px-1 top-[3px] left-[3px] rounded-full border-2 border-grey-500'>x</div>
 
                 <div className='mb-4'>
-                    <input className='w-[167px]' onChange={(e) => setFloorName(e.target.value)} value={floorName} type='text' />
+                    <input required className='w-[167px]' onChange={(e) => setFloorName(e.target.value)} value={floorName} type='text' />
                     <span className='text-white p-2'> {t("floor")}</span>
                 </div>
 
@@ -167,26 +163,26 @@ export default function EditModal({
     }
 
     return (
-        <div className='absolute z-10 p-4 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
+        <form onSubmit={handleUpdate} className='absolute z-10 p-4 rounded-lg  h-[196px] flex flex-col items-end left-[50%] w-[294px] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#33363F]'>
 
             <div onClick={() => setEdit(false)} className='absolute text-white cursor-pointer px-1 top-[3px] left-[3px] rounded-full border-2 border-grey-500'>x</div>
 
             <div className='mb-4'>
-                <input className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
+                <input required className='w-[167px]' onChange={(e) => setNewName(e.target.value)} value={newName} type='text' />
                 <span className='text-white p-2'>{t("name")}</span>
             </div>
 
             <div className=''>
-                <input className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
+                <input required className='w-[167px]' onChange={(e) => setCode(e.target.value)} value={newCode} type='text' />
                 <span className='text-white text-xs p-2'>{t("office number")}</span>
             </div>
 
 
-            <div onClick={handleUpdate} className='absolute bottom-[5px] right-[5px] text-white'>
+            <button type='submit' className='absolute bottom-[5px] right-[5px] text-white'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
-            </div>
-        </div>
+            </button>
+        </form>
     )
 }
